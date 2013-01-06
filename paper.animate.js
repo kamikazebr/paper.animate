44b689d413d89115d2e65eae29a5c07992755528
Jordan Wallwork <j_wallwork@hotmail.co.uk>
2013-01-06 20:09:00 +0000

    Begin adding scale and shear (neither working!)
    Implemented slightly hacky method to imitate "typed" function overloads

--- paper.animate.js
+++ paper.animate.js
@@ -28,6 +28,25 @@ PaperAnimate.AnimationProxy = (function() {
 		}
 	}
 	
+	AnimationProxy.prototype.scale = function() {
+		PaperAnimate.utils.selectOverload(
+			this, arguments,
+			[{
+				params: ["Number","Point","Number"],
+				fn: function(scale, center, _duration) {
+					this.modifiers.push(new PaperAnimate.modifiers.ScaleModifier(scale, scale, center, _duration || duration, this));
+				}
+			},
+			{
+				params: ["Number","Number","Point","Number"],
+				fn: function(hor, ver, center, _duration) {
+					this.modifiers.push(new PaperAnimate.modifiers.ScaleModifier(hor, ver, center, _duration || duration, this));
+				}
+			}]
+		);
+		return this;
+	}
+	
 	AnimationProxy.prototype.translate = function(point, _duration) {
 		this.modifiers.push(new PaperAnimate.modifiers.TranslateModifier(point, _duration || duration, this));
 		return this;
@@ -38,12 +57,50 @@ PaperAnimate.AnimationProxy = (function() {
 		return this;
 	}
 	
+	AnimationProxy.prototype.shear = function() {
+		PaperAnimate.utils.selectOverload(
+			this, arguments,
+			[{
+				params: ["Point","Point","Number"],
+				fn: function(point, center, _duration) {
+					this.modifiers.push(new PaperAnimate.modifiers.ShearModifier({point:point}, center, _duration || duration, this));
+				}
+			},
+			{
+				params: ["Number","Number","Point","Number"],
+				fn: function(hor, ver, center, _duration) {
+					this.modifiers.push(new PaperAnimate.modifiers.ShearModifier({hor:hor,ver:ver}, center, _duration || duration, this));
+				}
+			}]
+		);
+		return this;
+	}
+	
 	return AnimationProxy;
 })();
 
 // Modifiers
 
 PaperAnimate.modifiers = {};
+
+PaperAnimate.modifiers.ScaleModifier = (function() {
+	function ScaleModifier(_hor, _ver, _center, _duration, _proxy) {
+		this.hor = _hor;
+		this.ver = _ver;
+		this.center = _center;
+		this.duration = _duration;
+		this.proxy = _proxy;
+	}
+	
+	ScaleModifier.prototype.update = function(e) {
+		if (this.duration <= 0) { this.proxy.removeModifier(this); return; }
+		// todo
+		this.duration -= e.delta;
+	}
+	
+	return ScaleModifier;
+})();
+
 
 PaperAnimate.modifiers.TranslateModifier = (function() {
 		
@@ -55,8 +112,7 @@ PaperAnimate.modifiers.TranslateModifier = (function() {
 	
 	TranslateModifier.prototype.update = function(e) {
 		if (this.duration <= 0) { this.proxy.removeModifier(this); return; }
-		var updatePercentage = e.delta / this.duration,
-			updatePoint = PaperAnimate.utils.multiplyPoint(this.point, updatePercentage);
+		var updatePoint = PaperAnimate.utils.multiplyPoint(this.point, e.delta / this.duration);
 		this.proxy.item.translate(updatePoint);
 		this.point = new paper.Point(this.point.x - updatePoint.x, this.point.y - updatePoint.y);
 		this.duration -= e.delta;
@@ -83,6 +139,33 @@ PaperAnimate.modifiers.RotateModifier = (function() {
 	return RotateModifier;
 })();
 
+PaperAnimate.modifiers.ShearModifier = (function() {
+	function ShearModifier(_options, _center, _duration, _proxy) {
+		this.center = _center;
+		this.duration = _duration;
+		this.proxy = _proxy;
+		
+		if (this.point !== undefined) {
+			this.point = PaperAnimate.utils.dividePoint(_options.point, _duration);
+		} else {
+			this.hor = _options.hor / _duration;
+			this.ver = _options.ver / _duration;
+		}
+	}
+	
+	ShearModifier.prototype.update = function(e) {
+		if (this.duration <= 0) { this.proxy.removeModifier(this); return; }
+		if (this.point !== undefined) {
+			this.proxy.item.shear(PaperAnimate.utils.multiplyPoint(this.point, e.delta), this.center);
+		} else {
+			this.proxy.item.shear(this.hor * e.delta, this.ver * e.delta, this.center);
+		}
+		this.duration -= e.delta;
+	}
+	
+	return ShearModifier;
+})();
+
 // Utils
 
 PaperAnimate.utils = {
@@ -99,5 +182,23 @@ PaperAnimate.utils = {
 		);
 	},
 	getX: function(val) { return val.x !== undefined ? val.x : val; },
-	getY: function(val) { return val.y !== undefined ? val.y : val; }
+	getY: function(val) { return val.y !== undefined ? val.y : val; },
+	selectOverload: function(that, args, overloads) {
+		var types = {
+			Number: function(val) { return !isNaN(parseFloat(val)) && isFinite(val); },
+			Point: function(val) { return val.x !== undefined && val.y !== undefined; }
+		};
+		for (var o = 0; o < overloads.length; o++) {
+			var overload = overloads[o],
+				matches = true;
+			if (args.length > overloads[o].params.length) continue;
+			for (var a = 0; a < args.length; a++) {
+				if (!types[overload.params[a]](args[a])) {
+					matches = false;
+					break;
+				};
+			}
+			if (matches) { return overload.fn.apply(that, args); }
+		}
+	}
 };
